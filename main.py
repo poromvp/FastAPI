b305826b39d5e291012d1b1ea5fd639cb768f227
poromvp <quangkietle382@gmail.com>
2026-07-31 18:48:14 +0700

    we can check model or value of variable

--- main.py
+++ main.py
@@ -1,40 +1,57 @@
+from enum import Enum
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/', description="This is the ours first route")
+
+@app.get("/", description="This is the ours first route")
 async def base_get_route():
-    return {
-        "message" : "hello world"
-    }
+    return {"message": "hello world"}
 
 
-@app.post('/')
+@app.post("/")
 async def post():
-    return {
-        "message" : "hello from the post route"
-    }
+    return {"message": "hello from the post route"}
 
-@app.put('/')
+
+@app.put("/")
 async def put():
-    return {
-        "message" : "hello from the put route"
-    }
+    return {"message": "hello from the put route"}
+
 
 @app.get("/users")
 async def list_users():
-    return {
-        "message" : "list users route"
-    }
+    return {"message": "list users route"}
+
 
 @app.get("/users/me")
-async def get_current_user(): 
-    return {
-        "message" : "this is the current user"
-    }
+async def get_current_user():
+    return {"message": "this is the current user"}
+
 
 @app.get("/users/{users_id}")
-async def get_users(users_id: str): 
-    return {
-        "users_id" : users_id
-    }
+async def get_users(users_id: str):
+    return {"users_id": users_id}
+
+
+class FoodEnum(str, Enum):
+    fruits = "fruits"
+    vegetables = "vegetables"
+    dairy = "dairy"
+
+
+@app.get("/food/{food_name}")
+async def get_food(food_name: FoodEnum):
+    if food_name == FoodEnum.vegetables:
+        return {
+            "food_name": food_name,
+            "message": "you are healthy",
+        }
+    if food_name.value == "fruits":
+        return {
+            "food_name": food_name,
+            "message": "you are still healthy, but like sweeet things",
+        }
+
+    
