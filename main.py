1e1aa4d5a58e635d0915093ea434af4fefb97b1f
poromvp <quangkietle382@gmail.com>
2026-07-31 18:21:19 +0700

    learn abou path params

--- main.py
+++ main.py
@@ -20,3 +20,15 @@ async def put():
     return {
         "message" : "hello from the put route"
     }
+
+@app.get("/items")
+async def list_items():
+    return {
+        "message" : "list items route"
+    }
+
+@app.get("/items/{items_id}")
+async def get_items(items_id):
+    return {
+        "item_id" : "items_id"
+    }
