70433fe0b5d9b65b767e45c6e24ff9fb575dd696
poromvp <quangkietle382@gmail.com>
2026-07-31 17:21:03 +0700

    basic fast api app with basic method

--- main.py
+++ main.py
@@ -2,8 +2,21 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
-async def root():
+@app.get('/', description="This is the ours first route")
+async def base_get_route():
     return {
         "message" : "hello world"
+    }
+
+
+@app.post('/')
+async def post():
+    return {
+        "message" : "hello from the post route"
+    }
+
+@app.put('/')
+async def put():
+    return {
+        "message" : "hello from the put route"
     }
