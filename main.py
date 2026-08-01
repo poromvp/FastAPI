67bfc4bc8334876b90430aea364f3ba32b3975bc
poromvp <quangkietle382@gmail.com>
2026-08-01 23:05:07 +0700

    basic about querry parameter

--- main.py
+++ main.py
@@ -58,3 +58,13 @@ async def get_food(food_name: FoodEnum):
         "food_name" : food_name,
         "message" : "i like chocolate milk"
     }
+
+
+fake_items_db = [
+    {"item_name" : "Foo"},
+    {"item_name" : "Bar"},
+    {"item_name" : "Baz"}
+]
+@app.get("/items")
+async def list_items(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip: skip + limit]
