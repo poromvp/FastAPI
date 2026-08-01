011f479dc6228176b1687d058d147bc32b7f2916
poromvp <quangkietle382@gmail.com>
2026-08-01 23:17:47 +0700

    optional parameter

--- main.py
+++ main.py
@@ -54,17 +54,19 @@ async def get_food(food_name: FoodEnum):
             "message": "you are still healthy, but like sweeet things",
         }
 
-    return{
-        "food_name" : food_name,
-        "message" : "i like chocolate milk"
-    }
+    return {"food_name": food_name, "message": "i like chocolate milk"}
 
 
-fake_items_db = [
-    {"item_name" : "Foo"},
-    {"item_name" : "Bar"},
-    {"item_name" : "Baz"}
-]
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
 @app.get("/items")
 async def list_items(skip: int = 0, limit: int = 10):
-    return fake_items_db[skip: skip + limit]
+    return fake_items_db[skip : skip + limit]
+
+
+@app.get("/items/{item_id}")
+async def get_item(item_id: str = "haha", q: str | None = None):
+    if q:
+        return {"item_id": item_id, "q": q}
+    return {"item_id": item_id}
