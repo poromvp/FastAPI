cd4fbe9ba16ee66704c947b50cc32ce4648ef4f1
poromvp <quangkietle382@gmail.com>
2026-08-01 23:34:58 +0700

    practice 1

--- main.py
+++ main.py
@@ -73,3 +73,12 @@ async def get_item(item_id: str = "haha", q: str | None = None, short: bool = Fa
     if not short:
         item.update({"description" : "aaaaaaaaaaaaaaaaa"})
     return item
+
+@app.get("users/{user_id}/items/{item_id}")
+async def get_user_item(user_id: int, item_id:str, q:str | None = None, short: bool = False):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+            item.update({"description" : "aaaaaaaaaaaaaaaaa"})
+    return item
