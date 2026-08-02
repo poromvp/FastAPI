b32a33c2112e8e137849a2f33b2898c0a788f727
poromvp <quangkietle382@gmail.com>
2026-08-02 23:23:24 +0700

    Lesson 2 - Exercise A

--- exercise/ex-pathprams.py
+++ exercise/ex-pathprams.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/books/{book_id}")
 async def get_book_by_id(book_id : int):
     return {"book_id" : book_id, "title": "Sách bí ẩn"}
+
+@app.get("/employees/{emp_id}")
+async def get_employees_by_id(emp_id: int):
+    return {"employee_id": emp_id, "status": "Active"}
