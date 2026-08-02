6a6b396f89e1bf933c7c125348e5b896851c8570
poromvp <quangkietle382@gmail.com>
2026-08-02 23:25:46 +0700

    Lesson 2 - Exercise B

--- exercise/ex-pathprams.py
+++ exercise/ex-pathprams.py
@@ -9,3 +9,7 @@ async def get_book_by_id(book_id : int):
 @app.get("/employees/{emp_id}")
 async def get_employees_by_id(emp_id: int):
     return {"employee_id": emp_id, "status": "Active"}
+
+@app.get("/categories/{category_name}/posts")
+async def get_category_by_posts(category_name:str):
+    return {"category": category_name, "message": "Danh sách bài viết của chuyên mục"}
