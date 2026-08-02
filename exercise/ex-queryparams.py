3af6a672f3496e9bf7ace6702f8cdff3141409b0
poromvp <quangkietle382@gmail.com>
2026-08-02 23:45:48 +0700

    Lesson 3 - Exercise E

--- exercise/ex-queryparams.py
+++ exercise/ex-queryparams.py
@@ -12,3 +12,7 @@ product = [
 @app.get("/product")
 async def get_product(skip:int = 0, limit:int =20):
     return product[skip:skip+limit]
+
+@app.get("/search") #querry params không gắn giá trị mặc định thì nghĩa là bắt buộc
+async def get_search(keyword:str, is_active:bool | None = None): 
+    return {"search key word" : keyword, "is_active" : is_active, "message" : f"đây là kết quả tìm kiếm cho {keyword}, trạng thái active: {is_active}"}
