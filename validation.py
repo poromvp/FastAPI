76e8fd111d0eea9320334e7e0670f9afdc09bd42
poromvp <quangkietle382@gmail.com>
2026-08-05 23:19:42 +0700

    validation query in fastapi

---
+++ validation.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI, Query
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Item (BaseModel):
+    student_id : str
+    name: str
+    grade: int | None = None
+
+@app.get("/items")
+async def read_items(q: str | None = Query(None, max_length=10)):
+    results = {"items" : "baa"}
+    if q:
+        results.update({"q" : q})
+    return results
