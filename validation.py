ea87cc7d38df59d2a52300d804fab690d59e2556
poromvp <quangkietle382@gmail.com>
2026-08-05 23:54:11 +0700

    description and title in Query()

--- validation.py
+++ validation.py
@@ -3,15 +3,24 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-class Item (BaseModel):
-    student_id : str
+
+class Item(BaseModel):
+    student_id: str
     name: str
     grade: int | None = None
 
+
 @app.get("/items")
-async def read_items(q: list[str] = Query(["hi", "hello"])):
+async def read_items(
+    q: str | None = Query(
+        None, 
+        min_length=3, 
+        max_length=10, 
+        title="Sample query string",
+        description="This is a sample query string"),
+    ):
     #         dict[key, value có thể là str hoặc list[str]]
-    results : dict[str, str| list[str]] = {"items" : "baa"}
+    results: dict[str, str | list[str]] = {"items": "baa"}
     if q is not None:
-        results.update({"q" : q})
+        results.update({"q": q})
     return results
