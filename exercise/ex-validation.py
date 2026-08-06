723f948e4182c55cda25c1dc4553998e2de41a73
poromvp <quangkietle382@gmail.com>
2026-08-06 23:42:41 +0700

    Lesson 5 - Exercise B

--- exercise/ex-validation.py
+++ exercise/ex-validation.py
@@ -3,6 +3,12 @@ from pydantic import BaseModel
 
 app = FastAPI(title="BookStore API")
 
+#a)
 @app.get("/")
 async def root():
     return {"message" : "Chào mừng đến với cửa hàng sách"}
+
+#b)
+@app.get("/books/{book_id}")
+async def get_book_by_id(book_id:int):
+    return {"book_id" : book_id, "status": "available"}
