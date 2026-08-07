d6ebe3aed234a07b57d081452c2c93a68fd679a5
poromvp <quangkietle382@gmail.com>
2026-08-07 22:39:06 +0700

    fix exercise J

--- exercise/ex-validation.py
+++ exercise/ex-validation.py
@@ -3,14 +3,14 @@ from pydantic import BaseModel
 
 app = FastAPI(title="BookStore API")
 
+
 # e)
 class Book(BaseModel):
-    title:str
-    author:str
-    price:float
-    description:str | None = None
+    title: str
+    author: str
+    price: float
+    description: str | None = None
 
-    
 
 # a)
 @app.get("/")
@@ -33,40 +33,60 @@ async def getBookById(book_id: int):
 # d)
 @app.get("/books/")
 async def findBooks(skip: int = 0, limit: int = 10):
-    return {"skip": skip, "limit": limit, "message": f"Trả về {limit} sách, bỏ qua {skip} sách đầu."}
+    return {
+        "skip": skip,
+        "limit": limit,
+        "message": f"Trả về {limit} sách, bỏ qua {skip} sách đầu.",
+    }
 
 
 # e)
 @app.post("/books/")
-async def create_book(book:Book):
-    return {"message":"Sách đã được tạo thành công", "book_info" : book.model_dump()}
+async def create_book(book: Book):
+    return {"message": "Sách đã được tạo thành công", "book_info": book.model_dump()}
+
 
 # f)
 @app.put("/books/{book_id}")
-async def update_book(book_id:int, book:Book, is_discount:bool = False):
+async def update_book(book_id: int, book: Book, is_discount: bool = False):
     final_price = book.price * 0.9 if is_discount else book.price
     return {
         "book_id": book_id,
         "is_discount_applied": is_discount,
         "final_price": final_price,
-        "updated_data": book
+        "updated_data": book,
     }
+
 
 # g)
 @app.get("/search/")
-async def search_book(q:str = Query(..., min_length=3, max_length=50)):
-    return {"search query": q, "message" : "Đang tìm kiếm..."}
+async def search_book(q: str = Query(..., min_length=3, max_length=50)):
+    return {"search query": q, "message": "Đang tìm kiếm..."}
+
 
 # h)
 @app.get("/users/verify")
-async def verify_book(phone:str = Query(..., pattern=r"^0\d{9}$", description="Số điện thoại phải 10 số và bắt đầu bằng 0")):
+async def verify_book(
+    phone: str = Query(
+        ...,
+        pattern=r"^0\d{9}$",
+        description="Số điện thoại phải 10 số và bắt đầu bằng 0",
+    ),
+):
     return {"phone": phone, "status": "Hợp lệ"}
 
-# i)
+
+# i) j)
 @app.get("/filters")
 async def filter_books(
-    cat_id: int | None = Query(None, alias="category-id"), 
-    tags: list[str] = Query([]), 
-    debug_mode: bool = Query(False, include_in_schema=False)):
-    
-                       
+    cat_id: int | None = Query(None, alias="category-id"),
+    tags: list[str] = Query([]),
+    debug_mode: bool = Query(False, include_in_schema=False),
+):
+
+    result = {"category_id_received": cat_id, "tags": tags}
+
+    if debug_mode:
+        result["debug_info"] = "Bạn đã tìm ra tham số ẩn!"
+
+    return result
