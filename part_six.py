d206fc813af125166faf804f429ce18041a01076
poromvp <quangkietle382@gmail.com>
2026-08-09 23:42:15 +0700

    fix type of dict

--- part_six.py
+++ part_six.py
@@ -1,13 +1,11 @@
-from typing import Annotated
-
-from fastapi import FastAPI, Query
-#from pydantic import BaseModel
+from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/items/{item_id}")
-async def read_item_by_id(item_id: int, q:str | None = None):
-    result = {"item_id" : item_id}
+async def read_item_by_id(item_id: int, q: str | None = None):
+    result: dict[str, int | str] = {"item_id": item_id}
     if q:
         result.update({"q": q})
     return result
