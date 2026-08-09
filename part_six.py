b8ac48d785b804619435e367b4cc5b117f0fc331
poromvp <quangkietle382@gmail.com>
2026-08-09 23:47:02 +0700

    edit query of items

--- part_six.py
+++ part_six.py
@@ -1,10 +1,9 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Query
 app = FastAPI()
 
 
 @app.get("/items/{item_id}")
-async def read_item_by_id(item_id: int, q: str | None = None):
+async def read_item_by_id(item_id: int, q: str | None = Query(None, alias="item_query")):
     result: dict[str, int | str] = {"item_id": item_id}
     if q:
         result.update({"q": q})
