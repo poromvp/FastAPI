bdf5aa9954b56b09167afd847da2a00016ef8f8b
poro pc ubuntu <quangkietle382@gmail.com>
2026-08-10 22:26:24 +0700

    greater than equal, lower than equal

--- part_six.py
+++ part_six.py
@@ -4,7 +4,11 @@ app = FastAPI()
 
 
 @app.get("/items/{item_id}")
-async def read_item_by_id(*,item_id: int = Path(..., title="The ID of the item to get"), q: str):
+async def read_item_by_id(
+    *,
+    item_id: int = Path(..., title="The ID of the item to get", ge=10, le=100),
+    q: str
+    ):
     result: dict[str, int | str] = {"item_id": item_id}
     if q:
         result.update({"q": q})
