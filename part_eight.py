067828ffe105c2dc954ea4134cc5925e7a9be97c
poro pc ubuntu <quangkietle382@gmail.com>
2026-08-11 20:43:49 +0700

    Field have benifit  in schema

--- part_eight.py
+++ part_eight.py
@@ -4,12 +4,16 @@ Part 7 Body - Multiple Parameters
 
 from fastapi import FastAPI, Query, Path, Body
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
 class Item(BaseModel):
     name: str
+    description: str | None = Field(
+        None, title="The description of the item",
+        max_length=300
+    )
 
 @app.put("/items/{item_id}")
 async def update_item(
