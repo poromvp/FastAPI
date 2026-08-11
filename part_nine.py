2eba2099ad06e5adfe4f9e236ec390e43f02b582
poro pc ubuntu <quangkietle382@gmail.com>
2026-08-11 22:48:51 +0700

    set is using in matches value

--- part_nine.py
+++ part_nine.py
@@ -9,12 +9,12 @@ class Item(BaseModel):
     description: str | None = None
     price: float
     tax: float | None = None
-    tags: list[]
+    tags: set[str] = set()
     
 @app.put("/items/{item_id}")
 @app.put("/items/{item_id}")
 async def update_item(
     item_id: int, item: Item
     ):
-    results = {"item_id" : item_id, item:Item}
+    results = {"item_id" : item_id, "item":item}
     return results
