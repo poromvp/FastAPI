4dff8583f6932f754737534c204e92303f316e99
poromvp <quangkietle382@gmail.com>
2026-08-13 17:33:36 +0700

    Lesson 7 8 9 - Ex B

--- exercise/ex_nine.py
+++ exercise/ex_nine.py
@@ -1,6 +1,6 @@
 # Exercise 7 8 9
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 
 app = FastAPI()
 
@@ -9,3 +9,10 @@ app = FastAPI()
 async def read_root():
     return {"message" : "TechMart API"}
 
+#b)
+@app.get("/products/{product_id}")
+async def get_product_by_id(product_id:int = Path(..., ge=1, le=100000)):
+    return {"message" : f"lấy thành công sản phẩm có mã là {product_id}"}
+
+#c)
+@app.get("/produc")
