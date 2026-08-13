a09a0e0c3440c487105c165d49a2ebc92c452d18
poromvp <quangkietle382@gmail.com>
2026-08-13 17:39:39 +0700

    Lesson 7 8 9 - Ex C

--- exercise/ex_nine.py
+++ exercise/ex_nine.py
@@ -15,4 +15,7 @@ async def get_product_by_id(product_id:int = Path(..., ge=1, le=100000)):
     return {"message" : f"lấy thành công sản phẩm có mã là {product_id}"}
 
 #c)
-@app.get("/produc")
+@app.get("/categories/{category_slug}/products")
+async def get_products_by_category(category_slug:str = Path(..., min_length=3, max_length=30, regex='^[a-z0-9-]+$')):
+    return {"message" : f"Danh sách sản phẩm có category id là: {category_slug}"}
+                                   
