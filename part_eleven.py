4d7750b265bb4577c6bfd3ada08b1720540315b7
poro pc ubuntu <quangkietle382@gmail.com>
2026-08-16 19:24:04 +0700

    Lesson 11

---
+++ part_eleven.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, Query, Path, Body
+
+from pydantic import BaseModel, Field
+
+from uuid import UUID
+from datetime import datetime, time, timedelta
+
+app = FastAPI()
+
+# Part 11: Extra Data Type
+
+
+@app.put("/items/{item_id}")
+async def read_items(
+    item_id: UUID,
+    start_date: datetime | None = Body(None),
+    end_date: datetime | None = Body(None),
+    repeat_at: time | None = Body(None),
+    process_after: timedelta | None = Body(None),
+):
+    start_process = start_date + process_after
+    duration = end_date - start_process
+
+    return {
+        "item_id": item_id,
+        "start_date": start_date,
+        "end_date": end_date,
+        "repeat_at": repeat_at,
+        "process_after": process_after,
+        "start_process": start_process,
+        "duration": duration,
+    }
