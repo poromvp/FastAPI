0ae22080e3b7c7ac4ab106a3c83cfcc7911cd9c8
poromvp <quangkietle382@gmail.com>
2026-08-17 15:00:46 +0700

    Lesson 10-16 _ B

--- exercise/ex_sixteen.py
+++ exercise/ex_sixteen.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -9,4 +9,6 @@ async def root():
     return {"message" : "Chào mừng bạn đã đến với trang web"}
 
 #b
-@app.get
+@app.get("/books/{book_id}")
+async def get_book_by_id(book_id: int = Path(..., gt=0)):
+    return {"book_id" : book_id}
