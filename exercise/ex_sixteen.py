2ddc7ea9802ccb8999fb3730b44368f7d7bcacfa
poromvp <quangkietle382@gmail.com>
2026-08-17 15:11:44 +0700

    Lesson 10-16 _ C

--- exercise/ex_sixteen.py
+++ exercise/ex_sixteen.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, Query
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -12,3 +12,9 @@ async def root():
 @app.get("/books/{book_id}")
 async def get_book_by_id(book_id: int = Path(..., gt=0)):
     return {"book_id" : book_id}
+
+#c
+@app.get("/books/")
+async def get_books(skip:int = Query(0, gt=0), limit:int = Query(10, gt=0)):
+    limit = limit + skip
+    return {f"danh sach book tu {skip} den {limit}"}
