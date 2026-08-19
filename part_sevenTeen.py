18e152905f428581f1f2c70de230908ec3494689
poro pc ubuntu <quangkietle382@gmail.com>
2026-08-19 14:56:07 +0700

    File: a wild content, Upload File: include metadata of files

--- part_sevenTeen.py
+++ part_sevenTeen.py
@@ -12,7 +12,20 @@ from fastapi import (
     Query,
     Response,
     status,
+    File,
+    UploadFile,
 )
 from pydantic import BaseModel, Field
 
 app = FastAPI()
+
+
+# Part 17 Request File
+@app.post("/files/")
+async def create_file(f: bytes = File(...)):
+    return {"file": len(f)}
+
+
+@app.post("/upload-files/")
+async def create_upload_files(files: UploadFile):
+    return {"file-name": files.filename}
