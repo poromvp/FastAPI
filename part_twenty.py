e26e025431da9ebdd0de3838e50add483a056876
poro pc ubuntu <quangkietle382@gmail.com>
2026-08-27 15:50:07 +0700

    organization route with tags props

--- part_twenty.py
+++ part_twenty.py
@@ -12,6 +12,18 @@ class Item(BaseModel):
     tags: set[str] = set()
 
 
-@app.post("/items/", response_model=Item, status_code=status.HTTP_201_CREATED)
+@app.post(
+    "/items/", response_model=Item, status_code=status.HTTP_201_CREATED, tags=["items"]
+)
 async def create_item(item: Item):
     return item
+
+
+@app.get("/items/", tags=["items"])
+async def read_items():
+    return [{"name": "Foo", "price": 42}]
+
+
+@app.get("/users/", tags=["users"])
+async def read_users():
+    return [{"username": "Kim Sang Sik"}]
