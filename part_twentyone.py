8b25fe24318ad593919ca3897f57c60f0d27e87c
poro pc ubuntu <quangkietle382@gmail.com>
2026-08-27 23:46:35 +0700

    put method is gonna change all attribute

--- part_twentyone.py
+++ part_twentyone.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
 from datetime import datetime
 
@@ -8,14 +9,45 @@ fake_db = {}
 
 
 class Item(BaseModel):
-    title: str
-    timestap: datetime
+    name: str
     description: str | None = None
+    price: float
+    tax: float = 10.5
+    tags: set[str] = set()
 
 
-@app.put("/items/{id}")
-def update_item(id: str, item: Item):
-    json_compatible_item_data = jsonable_encoder(item)
-    fake_db[id] = json_compatible_item_data
-    print(fake_db)
-    return "Success"
+items = {
+    "Pyrus": {"name": "drago", "price": 100},
+    "Aquos": {
+        "name": "elfin",
+        "description": "day la mot guardiant bakugan",
+        "price": 180,
+        "tax": 20.2,
+    },
+    "Subterra": {
+        "name": "wilda",
+        "description": None,
+        "price": 150.2,
+        "tax": 10.2,
+        "tags": [],
+    },
+}
+
+# @app.put("/items/{id}")
+# def update_item(id: str, item: Item):
+#    json_compatible_item_data = jsonable_encoder(item)
+#    fake_db[id] = json_compatible_item_data
+#    print(fake_db)
+#   return "Success"
+
+
+@app.get("/items/{item_id}", response_model=Item)
+async def read_item(item_id: str):
+    return items.get(item_id)
+
+
+@app.put("/items/{item_id}")
+def update_bakugan(item_id: str, item: Item):
+    update_item_encoded = jsonable_encoder(item)
+    items[item_id] = update_item_encoded
+    return update_item_encoded
