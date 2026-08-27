c9cf9447bbc6cb4c4303670a853f74bba91dff47
poro pc ubuntu <quangkietle382@gmail.com>
2026-08-27 16:15:32 +0700

    Enum tags

--- part_twenty.py
+++ part_twenty.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, status
 from pydantic import BaseModel
+from enum import Enum
 
 app = FastAPI()
 
@@ -12,14 +13,22 @@ class Item(BaseModel):
     tags: set[str] = set()
 
 
+class Tags(Enum):
+    items = "items"
+    users = "users"
+
+
 @app.post(
-    "/items/", response_model=Item, status_code=status.HTTP_201_CREATED, tags=["items"]
+    "/items/",
+    response_model=Item,
+    status_code=status.HTTP_201_CREATED,
+    tags=[Tags.items],
 )
 async def create_item(item: Item):
     return item
 
 
-@app.get("/items/", tags=["items"])
+@app.get("/items/", tags=[Tags.items])
 async def read_items():
     return [{"name": "Foo", "price": 42}]
 
@@ -27,3 +36,10 @@ async def read_items():
 @app.get("/users/", tags=["users"])
 async def read_users():
     return [{"username": "Kim Sang Sik"}]
+
+
+# @app.get("/cates/{category}") Test Enum
+# async def get_cate(category: Tags):
+#    if category == Tags.items:
+#        return {"message": Tags.items}
+#    return {"message": Tags.users}
