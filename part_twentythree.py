2e554f5263d362235f6e20c0d0f7c175d3eecdfa
poro pc ubuntu <quangkietle382@gmail.com>
2026-08-28 19:18:55 +0700

    make class as dependencies

--- part_twentythree.py
+++ part_twentythree.py
@@ -5,3 +5,31 @@ app = FastAPI()
 
 
 # Part 23:Classes as Dependencies
+class Cat:
+    def __init__(self, name: str):
+        self.name = name
+
+
+fake_items_db = [{"item_name": "pyrus", "item_name": "aquos", "item_name": "ventus"}]
+
+
+class CommonQueryParams:
+    def __init__(
+        self,
+        q: Annotated[str | None, Query()] = None,
+        skip: Annotated[int, Query()] = 0,
+        limit: Annotated[int, Query()] = 100,
+    ):
+        self.q = q
+        self.skip = skip
+        self.limit = limit
+
+
+@app.get("/items/")
+async def read_items(commons: CommonQueryParams = Depends()):
+    response = {}
+    if commons.q:
+        response.update({"q": commons.q})
+    items = fake_items_db[commons.skip : commons.skip + commons.limit]
+    response.update({"item": items})
+    return response
