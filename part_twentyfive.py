8361b0f4d9cf98d6863fdb2e5f8f693e0d1dcd44
poro pc ubuntu <quangkietle382@gmail.com>
2026-08-29 00:34:56 +0700

    Part 25: Dependencies in path operation decorator , global

---
+++ part_twentyfive.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, Depends, Header, HTTPException
+from typing import Annotated
+
+
+async def verify_token(x_token: str = Header(...)):
+    if x_token != "fake-super-secret-token":
+        raise HTTPException(status_code=400, detail="X-Token header invalid")
+    return x_token
+
+
+async def verify_key(x_key: str = Header(...)):
+    if x_key != "fake-super-secret-key":
+        raise HTTPException(status_code=400, detail="X-Key header invalid")
+    return x_key
+
+
+app = FastAPI(dependencies=[Depends(verify_token), Depends(verify_key)])
+
+
+@app.get("/items")  # ,dependencies=[Depends(verify_token), Depends(verify_key)])
+async def read_items():
+    return [{"item": "Foo"}, {"item": "bar"}]
