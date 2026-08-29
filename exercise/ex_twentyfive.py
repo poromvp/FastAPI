f8ad07a8dca122b46c47b53ecbfb0bc896b1c3fd
poro pc ubuntu <quangkietle382@gmail.com>
2026-08-29 23:34:06 +0700

    test and check solutions

--- exercise/ex_twentyfive.py
+++ exercise/ex_twentyfive.py
@@ -26,18 +26,23 @@ app = FastAPI(
 
 
 class DeviceConfig(BaseModel):
-    ip_address: Annotated[str, Field()]
-    mac_address: Annotated[str, Field(..., pattern="MAC")]
+    ip_address: Annotated[str, Field(..., pattern=r"^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$")]
+    mac_address: Annotated[str, Field(..., min_length=7, max_length=17)]
     is_online: Annotated[bool, Field()] = False
 
 
 class DeviceCreate(BaseModel):
-    name: Annotated[str, Field(..., min_length=3)]
-    device_type: Annotated[str, Field()]
+    name: Annotated[str, Field(..., min_length=3, max_length=50)]
+    device_type: Annotated[str, Field(..., pattern=r"^(light|thermostat|camera)$")]
     config: Annotated[DeviceConfig, Field()]
 
 
 class RoomCreate(BaseModel):
-    room_name: Annotated[str, Field()]
+    room_name: Annotated[str, Field(..., min_length=3)]
     floor: Annotated[int, Field(..., ge=0)]
     devices: Annotated[list[DeviceCreate], Field()]
+
+
+@app.post("/test/")
+async def testing_model(device: RoomCreate):
+    return device
