a4d2784d82bf02d4f0f7291871e58b11b50c8a86
poro pc ubuntu <quangkietle382@gmail.com>
2026-08-31 00:05:34 +0700

    Ex 26 - A

---
+++ exercise/ex_twentysix.py
@@ -0,0 +1,6 @@
+from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import BaseModel
+
+app = FastAPI()
