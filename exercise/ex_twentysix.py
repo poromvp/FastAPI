de97574c02bef3286825c67cbf4c3be68256c9fd
poro pc ubuntu <quangkietle382@gmail.com>
2026-08-31 00:07:46 +0700

    Ex 26 - F

--- exercise/ex_twentysix.py
+++ exercise/ex_twentysix.py
@@ -23,3 +23,14 @@ def fake_decode_token(token: str):
     if token == "fake-super-secret-token":
         return {"username": "admin"}
     return None
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    user = fake_decode_token(token)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token không hợp lệ",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
