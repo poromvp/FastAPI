7185cbf11f107e6ec4e824f0ae2e93d3c82c0f80
poro pc ubuntu <quangkietle382@gmail.com>
2026-08-31 00:08:46 +0700

    Ex 26 - I

--- exercise/ex_twentysix.py
+++ exercise/ex_twentysix.py
@@ -44,3 +44,18 @@ async def read_users_me(current_user: dict = Depends(get_current_user)):
 @app.get("/public-data")
 async def read_public_data():
     return {"message": "Ai cũng xem được dữ liệu này"}
+
+
+@app.post("/login", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user_dict = fake_users_db.get(form_data.username)
+
+    # Kiểm tra user có tồn tại và password có đúng không
+    if not user_dict or form_data.password != user_dict["password"]:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Sai tên đăng nhập hoặc mật khẩu",
+        )
+
+    # Trả về token định dạng chuẩn
+    return {"access_token": "fake-super-secret-token", "token_type": "bearer"}
