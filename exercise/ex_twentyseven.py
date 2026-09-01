39d7ceef351bc610f89583fffdb75edf826539d9
poro pc ubuntu <quangkietle382@gmail.com>
2026-09-01 23:31:25 +0700

    install passlib

--- exercise/ex_twentyseven.py
+++ exercise/ex_twentyseven.py
@@ -1,1 +1,8 @@
-from fastapi import FastAPI
+from datetime import datetime, timedelta
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import BaseModel
+from passlib.context import CryptContext
+from jose import JWTError, jwt
+
+app = FastAPI()
