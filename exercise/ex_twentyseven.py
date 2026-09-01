8be5415aa9fb11027babd33c66af0476e0b467b2
poro pc ubuntu <quangkietle382@gmail.com>
2026-09-01 23:37:27 +0700

    Ex 27 - I

--- exercise/ex_twentyseven.py
+++ exercise/ex_twentyseven.py
@@ -70,3 +70,24 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
     # Tạo Token
     access_token = create_access_token(data={"sub": form_data.username})
     return {"access_token": access_token, "token_type": "bearer"}
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Không thể xác thực thông tin đăng nhập",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+
+    user_dict = fake_users_db.get(username)
+    if user_dict is None:
+        raise credentials_exception
+
+    return UserInDB(**user_dict)
