37bbae488ef36a07e9ff2d101dfc36024f03ac42
poro pc ubuntu <quangkietle382@gmail.com>
2026-09-01 23:35:08 +0700

    Ex 27 - D

--- exercise/ex_twentyseven.py
+++ exercise/ex_twentyseven.py
@@ -28,3 +28,8 @@ def verify_password(plain_password, hashed_password):
 
 def get_password_hash(password):
     return pwd_context.hash(password)
+
+
+SECRET_KEY = "day_la_mot_chuoi_bi_mat_cuc_ky_kho_doan"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
