a75bfb5b9e46c7cb7c0c2811b9b88b84d85a938b
poro pc ubuntu <quangkietle382@gmail.com>
2026-09-02 22:55:50 +0700

    Ex 27 - P

--- exercise/ex_twentyseven.py
+++ exercise/ex_twentyseven.py
@@ -147,3 +147,16 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends()):
 
     access_token = create_access_token(data={"sub": user.username, "role": user.role})
     return {"access_token": access_token, "token_type": "bearer"}
+
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise HTTPException(status_code=401, detail="Token không hợp lệ")
+        return payload
+    except ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail="Token đã hết hạn")
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Không thể xác thực token")
