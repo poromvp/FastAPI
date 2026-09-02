5001487d7701bc4f5397c237db8d03c8a4a62c78
poro pc ubuntu <quangkietle382@gmail.com>
2026-09-02 22:55:08 +0700

    Ex 27 - N

--- exercise/ex_twentyseven.py
+++ exercise/ex_twentyseven.py
@@ -130,3 +130,10 @@ def verify_password(plain_password, hashed_password):
 
 def get_password_hash(password):
     return pwd_context.hash(password)
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
