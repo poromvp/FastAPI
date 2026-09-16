35f9db00d82752718acd1c7c0519c1b952045f05
poro pc ubuntu <quangkietle382@gmail.com>
2026-09-16 23:04:35 +0700

    erase cite

--- exercise/ex_thirdty/main.py
+++ exercise/ex_thirdty/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-# Import các router từ các sub-module[cite: 1, 11]
+# Import các router từ các sub-module
 from routers import users, entries
 from internal import admin
 
@@ -8,7 +8,7 @@ app = FastAPI(
     title="E-Diary App", description="Ứng dụng chia nhỏ thành nhiều file (Part 30)"
 )
 
-# Nhúng (include) các router vào app chính[cite: 1]
+# Nhúng (include) các router vào app chính
 app.include_router(users.router)
 app.include_router(entries.router)
 app.include_router(admin.router)
