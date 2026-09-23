d6370d9d6502f54381b08bbd20e1786ee0fde8e6
poro pc ubuntu <quangkietle382@gmail.com>
2026-09-23 23:51:01 +0700

    log function

--- exercise/ex_31.py
+++ exercise/ex_31.py
@@ -6,3 +6,13 @@ app = FastAPI(
     title="Notification & Audit System",
     description="Thực hành Background Tasks và Dependency Injection (Part 31)",
 )
+
+
+def write_audit_log(action: str, user_id: int):
+    # time.sleep(3) giả lập việc xử lý nặng như gọi API bên thứ 3 hoặc gửi Email
+    time.sleep(3)
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    log_message = f"[{timestamp}] User {user_id}: {action}\n"
+
+    with open("audit_log.txt", mode="a", encoding="utf-8") as log_file:
+        log_file.write(log_message)
